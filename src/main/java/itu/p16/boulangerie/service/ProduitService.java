75284bfddfe32e0276a0e10f479a7543455d713c
OniAndria254 <154087569+OniAndria254@users.noreply.github.com>
2024-12-26 21:24:19 +0000

    back produits

---
+++ src/main/java/itu/p16/boulangerie/service/ProduitService.java
@@ -0,0 +1,24 @@
+package itu.p16.boulangerie.service;
+
+import itu.p16.boulangerie.entity.Produit;
+import itu.p16.boulangerie.entity.UniteMesure;
+import itu.p16.boulangerie.repository.ProduitRepository;
+import itu.p16.boulangerie.repository.UniteMesureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProduitService {
+    @Autowired
+    private ProduitRepository produitRepository;
+
+    public Produit save(Produit produit) {
+        return produitRepository.save(produit);
+    }
+
+    public List<Produit> getAll() {
+        return produitRepository.findAll();
+    }
+}
